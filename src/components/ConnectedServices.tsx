09d39e7bd48b3a6a39863dc03e016deeae306326
Israel-Felipe <israelfelipet@gmail.com>
2023-05-04 12:01:51 -0300

    refactor: Improve banner responsiveness

--- src/components/ConnectedServices.tsx
+++ src/components/ConnectedServices.tsx
@@ -33,29 +33,30 @@ export default function ConnectedServices() {
           />
         </motion.div>
         <motion.div
-          variants={textTransition(1)}
-          className="relative flex justify-center xl:justify-start sm:text-2xl tracking-widest font-normal text-white text-opacity-60 z-50"
+          variants={textTransition(0.7)}
+          className="relative flex xl:justify-start sm:text-[24px] px-12 md:px-20 xl:px-0
+          tracking-widest font-normal text-white text-opacity-60 z-50"
         >
           COMMUNITY-OWNED AND OPERATED
         </motion.div>
 
         <motion.div
-          variants={textTransition(0.7)}
-          className="relative flex justify-center font-Helvetica text-center lg:text-left 
+          variants={textTransition(0.4)}
+          className="relative flex justify-center font-Helvetica lg:text-left 
           text-[45px] sm:text-[80px] md:text-[90px]
-          leading-[60px] sm:leading-[100px]
-          px-5 md:px-40 xl:px-0 py-[50px]
+          leading-[52px] sm:leading-[100px]
+          px-12 md:px-20 xl:px-0 py-[30px] sm:py-[50px]
           bg-clip-text text-transparent bg-gradient-to-r from-white via white to-gray-500 z-50"
         >
           Enter a Universe of Connected Services.
         </motion.div>
 
         <motion.div
-          variants={textTransition(1)}
-          className="relative flex justify-center text-center xl:text-left tracking-[0.05em]
+          variants={textTransition(0.7)}
+          className="relative flex justify-center xl:text-left tracking-[0.05em]
           sm:text-xl md:text-3xl
           leading-7 lg:leading-[42px]
-          px-8 sm:px-16 md:px-28 lg:px-56 xl:pr-10 xl:pl-0"
+          px-12 sm:px-16 md:px-20 lg:px-56 xl:pr-10 xl:pl-0"
         >
           Cronos apps and services connect using IBC, the Inter-Blockchain
           Communication protocol. This innovation enables you to freely exchange
@@ -63,19 +64,22 @@ export default function ConnectedServices() {
         </motion.div>
 
         <motion.div
-          variants={textTransition(1)}
-          className="sm:flex sm:justify-center xl:justify-start
-          pt-12 gap-20 text-[20px]
+          variants={textTransition(0.7)}
+          className="flex xl:justify-start
+          px-12 md:px-20 lg:px-0 pt-12 gap-12 sm:gap-20 text-[16px] sm:text-[20px]
           "
         >
           <div className="flex justify-center">
-            <button className="flex items-center justify-center bg-gradient-to-r from-white via white to-gray-500 z-50 text-black w-[280px] h-[85px]">
+            <button
+              className="flex items-center justify-center bg-gradient-to-r from-white via white to-gray-500 z-50 text-black
+              w-[150px] sm:w-[280px] h-[60px] sm:h-[85px]"
+            >
               <span className="font-medium">Learn</span>
             </button>
           </div>
 
           <a
-            className="flex items-center justify-center pt-10 sm:pt-0 gap-4 z-50"
+            className="flex items-center justify-center gap-1 sm:gap-4 z-50"
             href="#"
           >
             <span className="font-medium">Explore Tokens</span>
@@ -88,13 +92,14 @@ export default function ConnectedServices() {
         initial="hidden"
         whileInView="show"
         viewport={{ once: false, amount: 0.25 }}
-        className="flex-col xl:w-2/5 text-center xl:text-right
+        variants={textTransition(0.7)}
+        className="xl:w-2/5 text-center xl:text-right
         pt-12 sm:pt-24"
       >
-        <motion.div variants={textTransition(1)}>
+        <motion.div>
           <h1
             className="relative font-Helvetica
-            text-[90px] sm:text-[100px] leading-[130px] font-black
+            text-[70px] sm:text-[100px] leading-[80px] sm:leading-[130px] font-black
             bg-clip-text text-transparent bg-gradient-to-r from-white via white to-gray-500 z-50"
           >
             265+
@@ -104,10 +109,10 @@ export default function ConnectedServices() {
           </span>
         </motion.div>
 
-        <motion.div variants={textTransition(1)} className="pt-10 sm:pt-36">
+        <motion.div className="pt-10 sm:pt-16 lg:pt-36">
           <h1
             className="relative font-Helvetica
-            text-[90px] sm:text-[100px] leading-[130px] font-black
+            text-[70px] sm:text-[100px] leading-[80px] sm:leading-[130px] font-black
             bg-clip-text text-transparent bg-gradient-to-r from-white via white to-gray-500 z-50"
           >
             $63B+
